7be3b1625486e7711c7804c247be7a3097ca9c49
jhinshawkcidev <62116651+jhinshawkcidev@users.noreply.github.com>
2020-03-19 12:25:12 -0700

    Update SPOScript-DocumentListItemFieldValues.ps1

--- SPOScript-DocumentListItemFieldValues.ps1
+++ SPOScript-DocumentListItemFieldValues.ps1
@@ -4,20 +4,21 @@
 
 Install-Module SharePointPnPPowerShellOnline
 $siteUrl  = "https://jhinshawkci.sharepoint.com"  # This is also the Credential, as long as it exists as a generic credential within your windows environment
-$listId = "List Name, ID, or Relative URL"
+$listId = "3632fe77-e755-4f08-945c-1d9bb839b11a"
 
-# Function to display field values of a list
 function DocumentListItems($TenantUrl) {
     Connect-PnPOnline -Url $TenantUrl -Credentials $TenantUrl
     $List = Get-PnPList -Identity $listId
-    $ListItems = (Get-PnPListItem -List $List -Id 1).FieldValues
-    foreach ($listItem in $ListItems) {
-        Write-Host $listItem
-    }
+    $FieldValues = (Get-PnPListItem -List $List).FieldValues
+
+    Write-Host "`t> Here is the List Item Title:" -ForegroundColor DarkYellow
+    Write-Host $FieldValues[0].Title -ForegroundColor Cyan
+
+    Write-Host "`t> Here is are the List Item Field Values:" -ForegroundColor DarkYellow
+    Write-Host $FieldValues[0] -ForegroundColor Blue
 }
 
-# Return sites - call function
-Write-Host "`t> Running Functions..." -ForegroundColor DarkYellow
+Write-Host "`t> Running Function..." -ForegroundColor DarkYellow
 $functionProduct  = DocumentListItems -TenantUrl $siteUrl
 
 Write-Host "`n`tComplete!`n" -ForegroundColor Green
