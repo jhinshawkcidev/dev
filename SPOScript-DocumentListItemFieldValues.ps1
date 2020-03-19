ae6315f3f85ae9f578ea12d911276127b5d762f3
jhinshawkcidev <62116651+jhinshawkcidev@users.noreply.github.com>
2020-03-19 12:39:53 -0700

    Update SPOScript-DocumentListItemFieldValues.ps1

--- SPOScript-DocumentListItemFieldValues.ps1
+++ SPOScript-DocumentListItemFieldValues.ps1
@@ -6,7 +6,7 @@ Install-Module SharePointPnPPowerShellOnline
 $siteUrl  = "https://jhinshawkci.sharepoint.com"  # This is also the Credential, as long as it exists as a generic credential within your windows environment
 $listId = "3632fe77-e755-4f08-945c-1d9bb839b11a"
 
-function DocumentListItems($TenantUrl) {
+function DocumentListItemFieldValues($TenantUrl) {
     Connect-PnPOnline -Url $TenantUrl -Credentials $TenantUrl
     $List = Get-PnPList -Identity $listId
     $FieldValues = (Get-PnPListItem -List $List).FieldValues
@@ -19,7 +19,7 @@ function DocumentListItems($TenantUrl) {
 }
 
 Write-Host "`t> Running Function..." -ForegroundColor DarkYellow
-$functionProduct  = DocumentListItems -TenantUrl $siteUrl
+$functionProduct  = DocumentListItemFieldValues -TenantUrl $siteUrl
 
 Write-Host "`n`tComplete!`n" -ForegroundColor Green
 [console]::beep(250,500)
